df03f0ae31d0a7530e33858004f336b5f5bbb0c8
Guilherme <gugafabrini@gmail.com>
2025-12-22 17:29:57 -0300

    ADD aula 57 - CSS de componentes

---
+++ 4_CSS_REACT/css-no-react/src/App.jsx
@@ -0,0 +1,21 @@
+// Imports em Geral
+
+// Imports de Estilos
+import './App.css'
+
+// Imports de Componentes
+import CSSDeComponente from './components/CSSDeComponente'
+
+function App() {
+
+  return (
+    <>
+      {/* CSS Global */}
+      <h1>React com CSS</h1>
+      {/* CSS de Componente */}
+      <CSSDeComponente />
+    </>
+  )
+}
+
+export default App
---
+++ 4_CSS_REACT/css-no-react/src/components/CSSDeComponente.jsx
@@ -0,0 +1,12 @@
+import './CSSDeComponente.css'
+
+const CSSDeComponente = () => {
+    return (
+        <div>
+            <h1>CSS De Componente</h1>
+            <p>Este é o parágrafo do componente</p>
+        </div>
+    )
+}
+
+export default CSSDeComponente
