1a3fc7b13b95a51931b04bc705126aa57cfe61ee
Guilherme <gugafabrini@gmail.com>
2025-12-23 11:55:22 -0300

    ADD: SECAO 5 @ 69 - Controlled inputs

--- 5_FORM_EM_REACT/form/src/App.jsx
+++ 5_FORM_EM_REACT/form/src/App.jsx
@@ -1,10 +1,15 @@
+// Import Estilo
 import './App.css'
+
+// Import Componentes
+import MeuFormulario from './components/MeuFormulario'
 
 function App() {
 
   return (
     <>
-      
+      <h2>Forms</h2>
+      <MeuFormulario user={{ nome: "Guilherme", email: "gugafabrini@gmail.com" }} />
     </>
   )
 }
---
+++ 5_FORM_EM_REACT/form/src/components/MeuFormulario.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import "./MeuFormulario.css"
+
+const MeuFormulario = ({user}) => {
+
+    // Inputs controlados
+
+
+    // Gerenciamento de dados
+    const [nome, setNome] = useState(user ? user.nome : "")
+    const [email, setEmail] = useState(user ? user.email : "")
+
+    const handleNome = (e) => {
+        setNome(e.target.value);
+    }
+
+    // Envio de form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Enviando formulário")
+        console.log(nome, email)
+    }
+
+    return (
+        <div>
+            {/* 1 - Criação de form */}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="nome">Nome</label>
+                    <input 
+                        type="text" 
+                        name="nome" 
+                        placeholder="Digite o seu nome" 
+                        onChange={handleNome} 
+                        value={nome}
+                        />
+                </div>
+                {/* Label envolvendo input */}
+                <label>
+                    <span>E-mail</span>
+                    {/* Simplificação de manipulação de state */}
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Digite o seu e-mail" 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        value={email}
+                        />
+                </label>
+                <input type="submit" value="Enviar" />
+            </form>
+        </div>
+    )
+}
+
+export default MeuFormulario
