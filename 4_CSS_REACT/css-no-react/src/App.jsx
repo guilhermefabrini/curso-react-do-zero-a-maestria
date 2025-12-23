7a1c74b4ab8e63c475066406e7f28ecb061b6450
Guilherme <gugafabrini@gmail.com>
2025-12-23 10:48:32 -0300

    Finaliza Seção 4_CSS_REACT

--- 4_CSS_REACT/css-no-react/src/App.jsx
+++ 4_CSS_REACT/css-no-react/src/App.jsx
@@ -1,12 +1,21 @@
 // Imports em Geral
+import { useState } from 'react'
 
 // Imports de Estilos
 import './App.css'
 
 // Imports de Componentes
 import CSSDeComponente from './components/CSSDeComponente'
+import CSSModules from './components/CSSModules'
 
 function App() {
+
+  // CSS Inline dinâmico
+  const n = 15
+  const [nome] = useState("Guilherme")
+  // ===================
+  //  Classes dinâmicas
+  const redTitle = true
 
   return (
     <>
@@ -14,6 +23,15 @@ function App() {
       <h1>React com CSS</h1>
       {/* CSS de Componente */}
       <CSSDeComponente />
+      {/* Inline CSS */}
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>Este elemento foi estilizado de forma inline.</p>
+      {/* CSS Inline dinâmico */}
+      <h2 style={n < 10 ? {color: "red"} : {color: "blue"}}>CSS Dinâmico</h2>
+      <h2 style={nome === "Guilherme" ? {color: "green"} : null}>Teste nome</h2>
+      {/* Classes Dinâmicas */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter classe dinâmica</h2>
+      {/* CSS Modules */}
+      <CSSModules />
     </>
   )
 }
---
+++ 4_CSS_REACT/css-no-react/src/components/CSSModules.jsx
@@ -0,0 +1,11 @@
+import styles from './CSSModules.module.css';
+
+const CSSModules = () => {
+    return (
+        <div>
+            <h1 className={styles.my_title}>Título Exemplo</h1>
+        </div>
+    )
+}
+
+export default CSSModules
