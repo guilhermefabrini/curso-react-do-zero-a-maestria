1827210021fb0b35a8d4cab5de741ad6221e6c14
Guilherme <gugafabrini@gmail.com>
2026-01-05 15:40:02 -0300

    78 - criando componentes do projeto

--- 6_PROJETO_SECRET_WORD/secretword/src/App.jsx
+++ 6_PROJETO_SECRET_WORD/secretword/src/App.jsx
@@ -1,14 +1,37 @@
+// CSS
 import './App.css'
 
-// Import de Componentes
-import StartScreen from './components/StartScreen';
+// React
+import { useCallback, useEffect, useState } from 'react';
 
+// Data
+import {wordsList} from "./data/words";
+
+// Componentes
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
+]
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  console.log(words)
+
   return (
     <div className='App'>
-      <StartScreen />
+      {/* Aqui, estamos dizendo que será exibido o componente 
+      de acordo com o valor dentro de 'gameStage' */}
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   )
 }
