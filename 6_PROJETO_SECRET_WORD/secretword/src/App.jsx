c674c4bd948cec4dee9202bc8dca5a6266820cc6
Guilherme <gugafabrini@gmail.com>
2026-01-07 16:49:20 -0300

    ADD: 87 - Finalizando tela de fim de jogo

--- 6_PROJETO_SECRET_WORD/secretword/src/App.jsx
+++ 6_PROJETO_SECRET_WORD/secretword/src/App.jsx
@@ -132,7 +132,7 @@ function App() {
           score={score}
        />
       )}
-      {gameStage === 'end' && <GameOver retry={retry} />}
+      {gameStage === 'end' && <GameOver retry={retry} score={score} />}
     </div>
   )
 }
