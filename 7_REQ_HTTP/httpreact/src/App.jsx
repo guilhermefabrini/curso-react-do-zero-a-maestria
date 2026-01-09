7333634b9a84a6d2b214a13dc088175fabc98dd0
Guilherme <gugafabrini@gmail.com>
2026-01-09 17:36:15 -0300

    ADD: 94 - Adicionando dados com React

--- 7_REQ_HTTP/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/src/App.jsx
@@ -7,16 +7,42 @@ const url = "http://localhost:3000/products"
 function App() {
   const [products, setProducts] = useState([])
 
-  // 1 - Resgatando dados
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+  // 1 - Resgatando dados (GET)
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url)
       const data = await res.json()
       setProducts(data)
-  }
-  fetchData();
-}, []);
+    }
+    fetchData();
+  }, []);
 
+  // 2 - Adição de produtos (POST)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+    // Normalmente, criamos um objeto para enviar os dados ao back-end
+    // Como a chave name e price tem o mesmo nome dos states, não é necessário fazer isso:
+    // name: name
+    // price: price
+    // Nesse caso, podemos fazer o objeto como está abaixo e o javascript liga uma coisa com a outra.
+    const products = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(products)
+    })
+
+  }
 
   console.log(products)
 
@@ -29,7 +55,20 @@ function App() {
             {product.name} - R$ {product.price}
           </li>
         ))}
-      </ul>   
+      </ul>  
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div> 
     </div>
   )
 }
