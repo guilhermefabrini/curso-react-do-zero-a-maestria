51bff051a21cf49ea523a296bfe838b370c40640
Guilherme <gugafabrini@gmail.com>
2026-01-12 14:56:01 -0300

    ADD: 100 - Tratando erros

--- 7_REQ_HTTP/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [products, setProducts] = useState([])
 
   // 4 - Custom hoook
-  const { data: items, httpConfig, loading } = useFetch(url);
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
   const [name, setName] = useState("")
   const [price, setPrice] = useState("")
@@ -67,7 +67,8 @@ function App() {
       <h1>Lista de Produtos</h1>  
        {/* 6 - loading  */}
        { loading && <p>Carregando dados...</p>}
-       { !loading && (
+       { error && <p>{error}</p> }
+       { !error && (
         <ul>
         {items && items.map((product) => (
           <li key={product.id}>
