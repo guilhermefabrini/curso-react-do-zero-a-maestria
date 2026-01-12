b0c76359645e2b3f40a66364653a8498aa208fe2
Guilherme <gugafabrini@gmail.com>
2026-01-12 11:53:17 -0300

    ADD: 99 - loading no post

--- 7_REQ_HTTP/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/src/App.jsx
@@ -86,7 +86,9 @@ function App() {
             Preço:
             <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
           </label>
-          <input type="submit" value="Criar" />
+          {/* 7 - Estado de loading no post */}
+          { loading && <input type="submit" disabled value="Aguarde" /> }
+          { !loading && <input type="submit" value="Criar" /> }
         </form>
       </div> 
     </div>
