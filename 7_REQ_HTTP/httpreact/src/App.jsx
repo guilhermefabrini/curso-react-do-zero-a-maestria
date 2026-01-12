840d875aa5c3222c5d4cd2df844b39919bfac7dc
Guilherme <gugafabrini@gmail.com>
2026-01-12 11:33:28 -0300

    ADD: 97 - Refatorando o post

--- 7_REQ_HTTP/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [products, setProducts] = useState([])
 
   // 4 - Custom hoook
-  const { data: items } = useFetch(url);
+  const { data: items, httpConfig } = useFetch(url);
 
   const [name, setName] = useState("")
   const [price, setPrice] = useState("")
@@ -40,19 +40,22 @@ function App() {
       price
     }
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(products)
-    })
-    // 3 - Carregamento dinâmico
-    // transforma a resposta em um objeto json contendo o item adicionao
-    const addedProduct = await res.json();
-    // o primeiro argumento de um 'set' é sempre o previous state, que nada mais é do que a versão atual do state.
-    // o que fazemos abaixo é pegar a versão atual da lista de produtos e adicionar à ela o valor novo cadastrado.
-    setProducts((prevProducts) => [...prevProducts, addedProduct])
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(products)
+    // })
+    // // 3 - Carregamento dinâmico
+    // // transforma a resposta em um objeto json contendo o item adicionado
+    // const addedProduct = await res.json();
+    // // o primeiro argumento de um 'set' é sempre o previous state, que nada mais é do que a versão atual do state.
+    // // o que fazemos abaixo é pegar a versão atual da lista de produtos e adicionar à ela o valor novo cadastrado.
+    // setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    // 5 - Refatorando POST
+    httpConfig(products, "POST")
 
     // limpa os campos do formulário após o cadastro
     setName("")
--- 7_REQ_HTTP/httpreact/src/hooks/useFetch.jsx
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.jsx
@@ -2,6 +2,25 @@ import {useState, useEffect} from 'react';
 
 export const useFetch = (url) => {
     const [data, setData] = useState(null);
+
+    // 5 - Refatorando o post
+    const [config, setConfig] = useState(null)
+    const [method, setMethod] = useState(null) 
+    const [callFetch, setCallFetch] = useState(false)
+
+    const httpConfig = (data, method) => {
+        if (method === "POST") {
+            setConfig({
+                method,
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            setMethod(method)
+        }
+    }
+
     useEffect(() => {
 
         const fetchData = async () => {
@@ -11,6 +30,18 @@ export const useFetch = (url) => {
         }
 
         fetchData()
-    }, [url]);
-    return { data };
+    }, [url, callFetch]);
+
+    // 5 - Refatorando post
+    useEffect(() => {
+        const httpRequest = async () => {
+            let fetchOptions = [url, config]
+            const res = await fetch(...fetchOptions)
+            const json = await res.json()
+            setCallFetch(json)
+        }
+        httpRequest()
+    }, [config, method, url])
+
+    return { data, httpConfig };
 }
