80bfef341810459d6678405e345d3f21945c38da
Guilherme <gugafabrini@gmail.com>
2026-01-13 10:15:05 -0300

    ADD: 105 - Configurando o React Router

---
+++ 8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+// Páginas
+import Home from './pages/Home'
+import About from './pages/About'
+
+
+// 1 - Configuração React Router
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>    
+    </>
+  )
+}
+
+export default App
