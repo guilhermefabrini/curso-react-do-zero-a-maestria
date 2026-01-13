38a2252c98d1a1d1c71e6b73a8061488fed3aa28
Guilherme <gugafabrini@gmail.com>
2026-01-13 10:29:07 -0300

    ADD: 106 - Navegação entre páginas

--- 8_REACT_ROUTER/reactrouter/src/App.jsx
+++ 8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 
+// Componentes
+import Navbar from './components/Navbar'
+
 
 // 1 - Configuração React Router
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
@@ -14,6 +17,7 @@ function App() {
     <>
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar/> 
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
