64a6e26a02165d9c77ecbb01c9816304c89ca437
Guilherme <gugafabrini@gmail.com>
2026-01-13 11:30:26 -0300

    ADD: 111 - Página 404

--- 8_REACT_ROUTER/reactrouter/src/App.jsx
+++ 8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Product from './pages/Product'
 import Info from './pages/Info'
+import NotFound from './pages/NotFound'
 
 // Componentes
 import Navbar from './components/Navbar'
@@ -27,6 +28,8 @@ function App() {
           <Route path='/products/:id' element={<Product />} />
           {/* 6 - nested route */}
           <Route path='/products/:id/info' element={<Info />} />
+          {/* 7 - No Match Route */}
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>    
     </>
