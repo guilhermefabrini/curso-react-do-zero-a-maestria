af10221ff2ea72b12398faaa9b11987c356a467f
Guilherme <gugafabrini@gmail.com>
2026-01-13 14:36:57 -0300

    ADD: 112 - Link ativo

--- 8_REACT_ROUTER/reactrouter/src/components/Navbar.jsx
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">Sobre</Link>
+            {/* Exemplo uso Link: */}
+            {/* <Link to="/">Home</Link>
+            <Link to="/about">Sobre</Link> */}
+            {/* NavLink: */}
+            <NavLink 
+            to="/"
+            // Exemplo de manipulação do isActive:
+            // className={({isActive}) => (isActive ? 'Está ativo' : 'Não está ativo ')}
+            >Home</NavLink>
+            <NavLink to="/about">Sobre</NavLink>
         </nav>
     )
 }
