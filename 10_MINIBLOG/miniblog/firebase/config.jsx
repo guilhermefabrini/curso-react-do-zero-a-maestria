666e736dc9e0899e7d78c5606da8d5925e7b84a5
Guilherme <gugafabrini@gmail.com>
2026-01-14 14:18:38 -0300

    ADD: 128 - Integrando o Firebase ao React

---
+++ 10_MINIBLOG/miniblog/firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdVX0LzDl8P6oE6vU5EOhrICEjh5MR4Tk",
+  authDomain: "miniblog-19aab.firebaseapp.com",
+  projectId: "miniblog-19aab",
+  storageBucket: "miniblog-19aab.firebasestorage.app",
+  messagingSenderId: "10466386793",
+  appId: "1:10466386793:web:f7e37be27c70220b8b53f8"
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export { db }
