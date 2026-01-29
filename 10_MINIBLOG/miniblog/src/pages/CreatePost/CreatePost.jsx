7024948439344e8abb04934b60640a33bd11ec1c
Guilherme <gugafabrini@gmail.com>
2026-01-29 15:19:08 -0300

    148 - Inserindo dados no firebase

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -3,6 +3,7 @@ import styles from "./CreatePost.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument"
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -11,8 +12,32 @@ const CreatePost = () => {
   const [formError, setFormError] = useState("");
   const [tags, setTags] = useState([]);
 
+  const { user } = useAuthValue()
+
+  const { insertDocument, response } = useInsertDocument("posts")
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("")
+
+    // validar URL da imagem
+
+    // criar o array de tags
+
+    // checar todos os valores
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName
+    })
+
+    // redirecionar para homepage
+
+
   };
 
   return (
@@ -63,14 +88,13 @@ const CreatePost = () => {
             value={tags}
           />
         </label>
-        <button className="btn">Cadastrar</button>
-        {/* {!loading && <button className="btn">Cadastrar </button>}
-        {loading && (
+        {!response.loading && <button className="btn">Cadastrar </button>}
+        {response.loading && (
           <button className="btn" disabled>
             Aguarde...{" "}
           </button>
         )}
-        {error && <p className="error">{error}</p>} */}
+        {response.error && <p className="error">{response.error}</p>}
       </form>
     </div>
   );
