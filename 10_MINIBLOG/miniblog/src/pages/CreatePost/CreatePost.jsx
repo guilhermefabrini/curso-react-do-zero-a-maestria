0a57019680e17e01046aba81b536c35b2f85d5f2
Guilherme <gugafabrini@gmail.com>
2026-01-30 10:33:30 -0300

    ADD: 149 - Validação dos dados

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -16,27 +16,39 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts")
 
+  const navigate = useNavigate()
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("")
 
     // validar URL da imagem
-
+    try {
+      new URL(image)
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.")
+    }
     // criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase())
 
     // checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos.")
+    }
+
+    if (formError) return
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createdBy: user.displayName
     })
 
     // redirecionar para homepage
-
+    navigate("/")
 
   };
 
@@ -95,6 +107,7 @@ const CreatePost = () => {
           </button>
         )}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
