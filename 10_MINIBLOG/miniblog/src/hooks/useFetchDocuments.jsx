9b791b86f109ddd24ee6d1df5a600062d07fc2ac
Guilherme Almeida <gugafabrini@gmail.com>
2026-02-06 11:20:24 -0300

    ADD: 157 - Exibindo dado de post individual

--- 10_MINIBLOG/miniblog/src/hooks/useFetchDocuments.jsx
+++ 10_MINIBLOG/miniblog/src/hooks/useFetchDocuments.jsx
@@ -49,7 +49,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
         loadData()
         //esse loadData eu tinha esquecido
 
-    }, [docCollection, search, uid, cancelled])   
+    }, [docCollection, documents, search, uid, cancelled])   
 
     useEffect(() => {
         return () => setCancelled(true)
