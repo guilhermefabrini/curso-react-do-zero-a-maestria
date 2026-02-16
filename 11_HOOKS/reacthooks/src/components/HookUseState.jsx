a408d30b2916bb6f5c8637e626e4c7a1527945a9
Guilherme Almeida <gugafabrini@gmail.com>
2026-02-16 10:52:33 -0300

    ADD: 169 - useState e inputs

--- 11_HOOKS/reacthooks/src/components/HookUseState.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseState.jsx
@@ -6,9 +6,18 @@ const HookUseState = () => {
     const [name, setName] = useState("Guilherme")
     
     const changeNames = () => {
-        userName = "João Souza"
+        // userName = "João Souza"
         setName("Guilherme Almeida")
         console.log(name, userName)
+    }
+
+    // 2 - useState e input
+    const [age, setAge] = useState(18)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // envio a uma api
+        console.log(age)
     }
 
     return (
@@ -18,6 +27,12 @@ const HookUseState = () => {
            <p>Variável: {userName}</p>
            <p>useState: {name}</p>
            <button onClick={changeNames}>Mudar nomes!</button>
+           {/* 2 - useState e input */}
+           <p>Digite a sua idade:</p>
+           <form onSubmit={handleSubmit}>
+            <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+           </form>
+           <p>Você tem {age} anos.</p>
            <hr />
         </div>
     )
