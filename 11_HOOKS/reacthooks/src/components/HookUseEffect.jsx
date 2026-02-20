61a28c14efc2d9cf5e2bee86d5cc0109e9d74761
Guilherme Almeida <gugafabrini@gmail.com>
2026-02-20 20:16:15 -0300

    ADD: 173 - Array de dependencias vazio no useEffect

--- 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
@@ -13,6 +13,11 @@ const HookUseEffect = () => {
         setNumber(number + 1)
     }
 
+    // 2 - Exemplo com array de dependências vazio
+    useEffect(() => {
+        console.log("Serei executado apenas uma vez")
+    }, [])
+
     return (
         <div>
             <h2>useEffect</h2>
