e6ab78a52a0c0b2bfb550f575b0e6d9eee7bc5a1
Guilherme Almeida <gugafabrini@gmail.com>
2026-02-22 12:08:53 -0300

    ADD: 175 - Cleanup no useEffect

--- 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
@@ -1,43 +1,55 @@
 import { useEffect, useState } from 'react'
 
 const HookUseEffect = () => {
+  // 1 - useEffect sem dependências
+  useEffect(() => {
+    console.log("Estou sendo executado");
+  });
 
-    // 1 - useEffect sem dependências
-    useEffect(() => {
-        console.log('Estou sendo executado')
-    })
+  const [number, setNumber] = useState(1);
 
-    const [number, setNumber] = useState(1)
+  const changeSomething = () => {
+    setNumber(number + 1);
+  };
 
-    const changeSomething = () => {
-        setNumber(number + 1)
+  // 2 - Exemplo com array de dependências vazio
+  useEffect(() => {
+    console.log("Serei executado apenas uma vez");
+  }, []);
+
+  // 3 - Utilizando o array de dependências
+  const [anotherNumber, setAnotherNumber] = useState(0);
+
+  useEffect(() => {
+    if (anotherNumber > 0) {
+      console.log("Sou executado apenas quando anotherNumber muda");
     }
+  }, [anotherNumber]);
 
-    // 2 - Exemplo com array de dependências vazio
-    useEffect(() => {
-        console.log("Serei executado apenas uma vez")
-    }, [])
+  // 4 - Limpeza do useEffect
+  // Alguns efeitos precisam ter uma técnica de cleanup (limpeza);
+  // Não fazer isso pode gerar erros ou comportamentos indesejados;
+  // Exemplo: um timeout que ao mudar de página pode continuar a ser executado pela falta desta limpeza;
+  useEffect(() => {
+    // const timer = setTimeout(() => {
+    //     // console.log("Hello World")
+    //     setAnotherNumber(anotherNumber + 1)
+    // }, 2000)
+    // return () => clearTimeout(timer)
+  }, [anotherNumber])
 
-    // 3 - Utilizando o array de dependências
-    const [anotherNumber, setAnotherNumber] = useState(0)
 
-    useEffect(() => {
-
-        if (anotherNumber > 0) {
-            console.log("Sou executado apenas quando anotherNumber muda")
-        }
-
-    }, [anotherNumber])
-
-    return (
-        <div>
-            <h2>useEffect</h2>
-            <p>Number: {number}</p>
-            <button onClick={changeSomething}>Executar!</button>
-            <p>Another Number: {anotherNumber}</p>
-            <button onClick={() => setAnotherNumber(anotherNumber + 1)}>Mudar another number</button>
-        </div>
-    )
+  return (
+    <div>
+      <h2>useEffect</h2>
+      <p>Number: {number}</p>
+      <button onClick={changeSomething}>Executar!</button>
+      <p>Another Number: {anotherNumber}</p>
+      <button onClick={() => setAnotherNumber(anotherNumber + 1)}>
+        Mudar another number
+      </button>
+    </div>
+  );
 }
 
 export default HookUseEffect
