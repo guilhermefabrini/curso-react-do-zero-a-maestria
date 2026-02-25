b3f4c2bce284fad6aaa77f3f4fcdbab377925d5f
Guilherme <gugafabrini@gmail.com>
2026-02-25 09:57:28 -0300

    ADD: 178 - useRef e DOM

--- 11_HOOKS/reacthooks/src/components/HookUseRef.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseRef.jsx
@@ -4,31 +4,57 @@
  * um objeto e seu valor fica na propriedade 'current'.
  * - Outra particularidade é que ele não re-renderiza o componente ao ser alterado, sendo interessante em determinadas situações.
  * Veja abaixo um exemplo de uso.
+ *
+ * useRef e DOM
+ *
+ * O useRef também pode ser utilizado para selecionar elementos no JSX, permitindo fazer manipulação de
+ * DOM ou aplicar funções como a focus (que foca o input).
  */
 
-import { useRef, useEffect, useState } from "react"
+import { useRef, useEffect, useState } from "react";
 
 const HookUseRef = () => {
+  const numberRef = useRef(0);
+  const [counter, setCounter] = useState(0);
+  const [counterB, setCounterB] = useState(0);
 
-    const numberRef = useRef(0)
-    const [counter, setCounter] = useState(0)
-    const [counterB, setCounterB] = useState(0)
+  useEffect(() => {
+    numberRef.current = numberRef.current + 1;
+  });
 
-    useEffect(() => {
-        numberRef.current = numberRef.current + 1
-    })
+  // 2 - useRef e DOM
+  const inputRef = useRef();
+  const [text, setText] = useState("");
 
-    return (
-        <div>
-            <h2>useRef</h2>
-            <p>O componente renderizou: {numberRef.current} vezes.</p>
-            <p>Counter 1: {counter}</p>
-            <button onClick={() => setCounter(counter + 1)}>Contador A</button>
-            <p>Counter 2: {counterB}</p>
-            <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
-            <hr />
-        </div>
-    )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-export default HookUseRef
+    setText("")
+    inputRef.current.focus()
+  }
+
+  return (
+    <div>
+      <h2>useRef</h2>
+      {/* 1 - useRef */}
+      <p>O componente renderizou: {numberRef.current} vezes.</p>
+      <p>Counter 1: {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>Contador A</button>
+      <p>Counter 2: {counterB}</p>
+      <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+      {/* 2 - useRef e DOM */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={inputRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      <hr />
+    </div>
+  );
+};
+
+export default HookUseRef;
