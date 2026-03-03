3149963d03feb6618c12c0dcc1ec2b6eee6aac3f
Guilherme <gugafabrini@gmail.com>
2026-03-03 11:42:51 -0300

    ADD: 185 - useDebugValue && Conclui seção de 11_HOOKS

--- 11_HOOKS/reacthooks/src/hooks/usePrevious.jsx
+++ 11_HOOKS/reacthooks/src/hooks/usePrevious.jsx
@@ -1,11 +1,20 @@
-import { useEffect, useRef } from 'react' 
+/**
+ * useDebugValue
+ * 
+ * - É um hook utilizado para debug. Recomenda-se o uso justamente em custom hooks como este.
+ * - Ele adiciona uma área no React Dev Tools e estará no componente em que o hook é utilizado.
+ */
+
+import { useEffect, useRef, useDebugValue } from 'react' 
 
 export const usePrevious = (value) => {
     const ref = useRef;
 
+    useDebugValue("--- CUSTOM HOOK E USEDEBUGVALUE ---")
+    useDebugValue("O número anterior é: " + value)
+
     useEffect(() => {
         ref.current = value
-
     })
 
     return ref.current
