2af12a6636f4407495ff3714bcc356453450ef54
Guilherme Almeida <gugafabrini@gmail.com>
2026-03-14 11:42:49 -0300

    ADD: 210 - Rota de teste API

--- 13_REACTGRAM/backend/app.js
+++ 13_REACTGRAM/backend/app.js
@@ -12,6 +12,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+const router = require("./routes/Router")
+app.use(router)
+
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
 })
