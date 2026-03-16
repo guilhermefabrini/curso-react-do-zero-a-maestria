f1e432c67b3d2905cd6d1eef33379db33a6f2b87
Guilherme Almeida <gugafabrini@gmail.com>
2026-03-15 22:32:06 -0300

    ADD: 211 - Importando middlewares

--- 13_REACTGRAM/backend/app.js
+++ 13_REACTGRAM/backend/app.js
@@ -12,6 +12,15 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+// Resolver CORS
+app.use(cors({credentials: true, origin: "http://localhost:3000"}))
+
+// Diretório de upload
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+// DB connection
+require("./config/db.js")
+
 const router = require("./routes/Router")
 app.use(router)
 
