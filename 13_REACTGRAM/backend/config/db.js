a624ec86867ad6e44bbec7322a68ba02d40aa998
Guilherme <gugafabrini@gmail.com>
2026-03-17 14:17:36 -0300

    ADD: 213 - Criando model de usuário

--- 13_REACTGRAM/backend/config/db.js
+++ 13_REACTGRAM/backend/config/db.js
@@ -3,7 +3,7 @@ const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 
 // connection
-const conn = async () => {
+const conn = async () => {                                                                              
     try {
 
         const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.cv3meqn.mongodb.net/?appName=Cluster0`)
