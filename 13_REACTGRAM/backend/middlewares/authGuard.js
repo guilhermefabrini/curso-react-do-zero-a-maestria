1e24758a2f6bd8bc52098c0be71d64d034176a5a
Guilherme <gugafabrini@gmail.com>
2026-03-27 16:37:01 -0300

    ADD: 221 - Criando validação de autenticação

---
+++ 13_REACTGRAM/backend/middlewares/authGuard.js
@@ -0,0 +1,22 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const jwtSecret = process.env.JWT_SECRET
+
+const authGuard = async (req, res, next) => {
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1]
+
+    // Verifica se o header tem um token
+    if (!token) return res.status(401).json({errors: ['Acesso negado!']})
+    
+    // Verifica se o token é válido
+    try {
+        const verified = jwt.verify(token, jwtSecret)
+        req.user = await User.findById(verified.id).select("-password")
+        next()
+    } catch (error) {
+        res.status(401).json({errors: ['Token inválido.']})
+    }
+}
+
+module.exports = authGuard
