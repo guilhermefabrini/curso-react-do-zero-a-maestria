abb4145e8fd022e373fcb9a1dad3b33ee9d45593
Guilherme <gugafabrini@gmail.com>
2026-04-14 16:20:18 -0300

    ADD: 235 - Comentário em foto

--- 13_REACTGRAM/backend/controllers/PhotoController.js
+++ 13_REACTGRAM/backend/controllers/PhotoController.js
@@ -134,6 +134,36 @@ const likePhoto = async (req, res) => {
     
 }
 
+// Funcionalidade de comentário
+const commentPhoto = async (req, res) => {
+    const {id} = req.params
+    const {comment} = req.body
+    const reqUser = req.user
+    const user = await User.findById(reqUser._id)
+    const photo = await Photo.findById(id)
+
+    if (!photo) {
+        res.status(404).json({errors: ["Foto não encontrada"]})
+        return
+    }
+
+    // Coloca o comentário no array de comentários
+    const userComment = {
+        comment,
+        userName: user.name,
+        userImage: user.profileImage,
+        userId: user._id
+    }
+
+    photo.comments.push(userComment)
+    await photo.save()
+
+    res.status(200).json({
+        comment: userComment,
+        message: "O comentário foi adicionado com sucesso!"
+    })
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -141,5 +171,6 @@ module.exports = {
     getUserPhotos,
     getPhotoById,
     updatePhoto,
-    likePhoto
+    likePhoto,
+    commentPhoto
 }
