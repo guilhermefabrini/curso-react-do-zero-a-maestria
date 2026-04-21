d4a0a89a7f615cc3a5feae9f0d9fa660bb2a882a
Guilherme Almeida <gugafabrini@gmail.com>
2026-04-21 15:11:52 -0300

    ADD: 239 - Configurando React Router

--- 13_REACTGRAM/frontend/src/App.jsx
+++ 13_REACTGRAM/frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
 
+// router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home/Home'
+
 function App() {
 
   return (
     <div>
-        <h1>ReactGram</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
