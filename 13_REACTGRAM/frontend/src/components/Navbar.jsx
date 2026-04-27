d68ddf6cb1884cb1b59986f706f2687741b2a2ff
Guilherme <gugafabrini@gmail.com>
2026-04-27 16:35:52 -0300

    ADD: 243 - CSS global

--- 13_REACTGRAM/frontend/src/components/Navbar.jsx
+++ 13_REACTGRAM/frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <nav id="nav">
       <Link to="/">ReactGram</Link>
-      <form>
+      <form id="search-form">
         <BsSearch />
         <input type="text" placeholder="Pesquisar" />
       </form>
