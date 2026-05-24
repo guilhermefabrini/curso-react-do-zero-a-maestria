3acedba10092cf22c3e7488e6778cd89acd2be58
Guilherme Almeida <gugafabrini@gmail.com>
2026-05-23 21:43:49 -0300

    ADD: 254 - Verificando se usuário está autenticado

--- 13_REACTGRAM/frontend/src/App.jsx
+++ 13_REACTGRAM/frontend/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 // router
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
+// hooks
+import {useAuth} from './hooks/useAuth'
+
 // components
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -12,15 +15,30 @@ import Home from './pages/Home/Home'
 
 function App() {
 
+  const {auth, loading} = useAuth()
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div>
       <BrowserRouter>
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="login" />}
+            />
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
         <Footer />
